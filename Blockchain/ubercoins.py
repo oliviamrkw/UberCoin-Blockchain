cf332fdd464e2edd736518e82c331a396f25fec7
Olivia <98853542+oliviamrkw@users.noreply.github.com>
2023-09-20 18:32:40 -0400

    added comments

--- Blockchain/ubercoins.py
+++ Blockchain/ubercoins.py
@@ -1,11 +1,15 @@
-def calculate_ubercoins(distance): # calculates UberCoins based on distance
+# calculates UberCoins based on distance
+def calculate_ubercoins(distance): 
     return distance * 2.6 #2.6 ubercoins for 1 km
 
-def calculate_money(ubercoins): # calculates money based on amount of UberCoins
+# calculates money based on amount of UberCoins
+def calculate_money(ubercoins):
     return ubercoins * (120/26) # $120/26 for every ubercoin
 
-def add_id_to_list(id, dictionary): # adds a wallet id to a dictionary
+# adds a wallet id to a dictionary
+def add_id_to_list(id, dictionary):
     dictionary[id] = 0
 
-def add_ubercoins_to_id(id, ubercoins, dictionary): # adds UberCoins to a wallet ID in a dictionary
+# adds UberCoins to a wallet ID in a dictionary
+def add_ubercoins_to_id(id, ubercoins, dictionary):
     dictionary[id] += ubercoins
